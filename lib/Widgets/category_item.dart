cde8a7f99d290880f17e4ef9f78a2218643cdaf4
KrishKoria <krishkoria2004@gmail.com>
2024-01-08 23:02:00 +0530

    Created A New Project, Added a Categories selection screen, dummy data file and used inkwell to create a clickable widget.

---
+++ lib/Widgets/category_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/Models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem(this.category, {super.key});
+  final Category category;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
+  }
+}
